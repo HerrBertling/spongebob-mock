6be50b4e325d9fb8e7bf60b2e9c45914c8550964
Markus Siering <markus277@gmail.com>
2022-03-20 19:19:43 +0100

    chore: update some colors

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
       <h1 class="text-5xl font-bold text-pink-500 mb-12">SpOnGeBoBiFy!</h1>
       <form class="grid grid-cols-1 gap-4 mb-12" @submit.prevent="spongify">
         <label>
-          <span class="font-bold block text-lg text-gray-800">Your text</span>
-          <input v-model="sentence" placeholder="ScHrEiB wAs…" class="border rounded-sm border-gray-400 hover:border-blue-500 focus:border-blue-500 outline-none px-4 py-3 block w-full" />
+          <span class="font-bold block text-xl text-pink-500 mb-2">Your text</span>
+          <input v-model="sentence" placeholder="WrItE sOmEtHiNg…" class="border rounded-sm border-gray-400 hover:border-blue-500 focus:border-blue-500 outline-none px-4 py-3 mb-4 block w-full" />
         </label>
         <div>
           <button type="submit" class="text-white bg-pink-500 px-4 py-2 rounded-md text-lg hover:bg-pink-700 active:bg-pink-700 focus:bg-pink-700 transition-colors">
@@ -16,9 +16,9 @@
       <div v-if="spongedSentence" @click="copy">
         <label>
           <strong class="block text-pink-500 font-bold text-4xl mb-4">Happy trolling:</strong>
-          <textarea class="h-48 bg-pink-400 rounded-md p-4 text-xl w-full block focus:outline-none mb-4" ref="copyText">{{ spongedSentence }}</textarea>
+          <textarea class="h-48 bg-pink-300 rounded-md p-4 text-xl w-full block focus:outline-none mb-4 text-pink-900" ref="copyText">{{ spongedSentence }}</textarea>
         </label>
-        <p class="text-pink-500">Just click the pink field to copy the text to your clipboard.</p>
+        <p class="text-pink-400">Just click the pink field to copy the text to your clipboard.</p>
       </div>
     </main>
     <footer class="max-w-xl m-auto p-4">
